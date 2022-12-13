5e097572791ae70787290fbab0c2075cdd655908
parmarayushi <you@example.comaayushiparmar812@gmail.com>
2022-12-13 19:28:21 +0530

    feat: displayed data on dashboard.

--- src/app/dashboard/dashboard-container/dashboard-container.component.ts
+++ src/app/dashboard/dashboard-container/dashboard-container.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Employees } from 'src/app/employees/employee.model';
+import { Projects } from 'src/app/projects/project.model';
+import { CommonService } from 'src/app/shared/services/common.service';
+import { Task } from 'src/app/tasks/task.model';
+import { TaskService } from 'src/app/tasks/task.service';
 
 @Component({
   selector: 'app-dashboard-container',
@@ -6,9 +12,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardContainerComponent implements OnInit {
 
-  constructor() { }
+  public employeeData$: Observable<Employees[]>;
+  public projectData$: Observable<Projects[]>;
+  public taskData$: Observable<Task[]>;
 
-  ngOnInit(): void {
+  constructor(
+    private commonService: CommonService,
+    private taskService: TaskService
+  ) {
+    this.employeeData$ = new Observable();
+    this.projectData$ = new Observable();
+    this.taskData$ = new Observable();
   }
 
+  ngOnInit(): void {
+    this.props()
+  }
+
+  public props(){
+    this.employeeData$ = this.commonService.getEmployees();
+    this.projectData$ = this.commonService.getProjects();
+    this.taskData$ = this.taskService.getTask();
+  }
 }
--- src/app/dashboard/dashboard-container/dashboard-presentation/dashboard-presentation.component.ts
+++ src/app/dashboard/dashboard-container/dashboard-presentation/dashboard-presentation.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Employees } from 'src/app/employees/employee.model';
+import { Projects } from 'src/app/projects/project.model';
+import { Task } from 'src/app/tasks/task.model';
 
 @Component({
   selector: 'app-dashboard-presentation',
@@ -6,9 +9,82 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardPresentationComponent implements OnInit {
 
+  /**
+  * @name totalEmployees
+  * @description sets the data of employees.
+  */
+  @Input() public set totalEmployee(value: Employees[] | null) {
+    if (value) {
+      this._employees = value
+    }
+  }
+
+  /**
+  * @name totalEmployees
+  * @description gets the data of employees.
+  */
+  public get totalEmployees(): Employees[] {
+    return this._employees;
+  }
+
+  /**
+  * @name totalProjects
+  * @description sets the data of projects.
+  */
+  @Input() public set totalProjects(value: Projects[] | null) {
+    if (value) {
+      this._projects = value
+    }
+  }
+
+  /**
+  * @name totalProjects
+  * @description gets the data of projects.
+  */
+  public get totalProjects(): Projects[] {
+    return this._projects;
+  }
+
+  /**
+  * @name totalTasks
+  * @description sets the data of tasks.
+  */
+  @Input() public set totalTasks(value: Task[] | null) {
+    if (value) {
+      this._tasks = value
+      this.status();
+    }
+  }
+
+  /**
+  * @name totalTasks
+  * @description gets the data of the tasks.
+  */
+  public get totalTasks(): Task[] {
+    return this._tasks
+  }
+
+  public pending: Task[];
+  public inProgress: Task[];
+  public done: Task[];
+
+  private _employees: Employees[];
+  private _projects: Projects[];
+  private _tasks: Task[];
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  /**
+  * @name status
+  * @description filters the task with Pending, In Progress and Done .
+  */
+  public status() {
+    this.pending = this._tasks.filter((res) => res.status === "Pending");
+    this.inProgress = this._tasks.filter((res) => res.status === "In Progress");
+    this.done = this._tasks.filter((res) => res.status === "Done");
   }
 
 }
